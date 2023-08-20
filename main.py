2795b354a89fb9459874c8bf029a6ed535cb4d10
Dohyun Moon <d6moon@uwaterloo.ca>
2023-08-20 02:43:55 -0400

    multithreading key and mouse listeners

--- main.py
+++ main.py
@@ -1,6 +1,23 @@
 from pynput import mouse, keyboard
+import pygetwindow as gw
+import threading
+import psutil
 
-def main():
+current_word = 'a'
+
+def observers():
+    prev_active_window = gw.getActiveWindow()
+
+    while True:
+        active_window = gw.getActiveWindow()
+
+        if active_window and active_window != prev_active_window:
+            prev_active_window = active_window
+            print(f"{active_window.title} at {id(active_window)}")
+
+    return
+
+def listeners():
     mouse_listener = mouse.Listener(on_click=on_click_mouse)
     key_listener = keyboard.Listener(on_press=on_press_keyboard)
 
@@ -11,10 +28,16 @@ def main():
     key_listener.join()
 
 def on_press_keyboard(key):
-    print('{0}'.format(
-        key))
-    if key.char == 'x':
-        return False
+    try:
+        print('{0}'.format(
+            key))
+        global current_word
+        current_word = key.char
+        if key.char == 'x':
+            return False
+    except AttributeError:
+        print('{0}'.format(
+            key))
 
 def on_click_mouse(x, y, button, pressed):
     print('{0} {1} at {2}'.format(
@@ -26,4 +49,13 @@ def on_click_mouse(x, y, button, pressed):
         return False
 
 if __name__ == "__main__":
-    main()
+    thread1 = threading.Thread(target=listeners)
+    thread2 = threading.Thread(target=observers)
+
+    thread1.start()
+    thread2.start()
+
+    thread1.join()
+    thread2.join()
+
+    print("tasks done")

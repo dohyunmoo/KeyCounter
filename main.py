1e57b2c59d35a26daee56a257b9493327a8e347b
Dohyun Moon <d6moon@uwaterloo.ca>
2023-08-25 23:06:12 -0400

    adding data to json

--- main.py
+++ main.py
@@ -2,8 +2,84 @@ from pynput import mouse, keyboard
 import pygetwindow as gw
 import threading
 import psutil
+import json
+from datetime import datetime
 
-current_word = 'a'
+current_data = {
+    'lclick': 0,
+    'rclick': 0,
+    'a': 0,
+    'b': 0,
+    'c': 0,
+    'd': 0,
+    'e': 0,
+    'f': 0,
+    'g': 0,
+    'h': 0,
+    'i': 0,
+    'j': 0,
+    'k': 0,
+    'l': 0,
+    'm': 0,
+    'n': 0,
+    'o': 0,
+    'p': 0,
+    'q': 0,
+    'r': 0,
+    's': 0,
+    't': 0,
+    'u': 0,
+    'v': 0,
+    'w': 0,
+    'x': 0,
+    'y': 0,
+    'z': 0,
+    '`': 0,
+    'F1': 0,
+    'F2': 0,
+    'F3': 0,
+    'F4': 0,
+    'F5': 0,
+    'F6': 0,
+    'F7': 0,
+    'F8': 0,
+    'F9': 0,
+    'F10': 0,
+    'F11': 0,
+    'F12': 0,
+    'TAB': 0,
+    'CAPSLOCK': 0,
+    'SHIFT': 0,
+    'CTRL': 0,
+    'ALT': 0,
+    'ESC': 0,
+    'ENTER': 0,
+    'UP': 0,
+    'DOWN': 0,
+    'LEFT': 0,
+    'RIGHT': 0,
+    'PRINTSCREEN': 0,
+    'PAUSE': 0,
+    'INSERT': 0,
+    'HOME': 0,
+    'PAGEUP': 0,
+    'PAGEDOWN': 0,
+    'DELETE': 0,
+    'END': 0,
+    'SCROLLLOCK': 0,
+    'BACKSPACE': 0,
+    '-': 0,
+    '=': 0,
+    '[': 0,
+    ']': 0,
+    '\\': 0,
+    ';': 0,
+    '\'': 0,
+    ',': 0,
+    '.': 0,
+    '/': 0,
+    'SPACE': 0
+}
 
 def observers():
     prev_active_window = gw.getActiveWindow()
@@ -29,15 +105,14 @@ def listeners():
 
 def on_press_keyboard(key):
     try:
-        print('{0}'.format(
-            key))
-        global current_word
-        current_word = key.char
+        print(f"{key.char}: {ord(key.char)}")
         if key.char == 'x':
             return False
-    except AttributeError:
+    except:
         print('{0}'.format(
             key))
+        
+    analyze_keypress(key)
 
 def on_click_mouse(x, y, button, pressed):
     print('{0} {1} at {2}'.format(
@@ -49,14 +124,26 @@ def on_click_mouse(x, y, button, pressed):
         return False
     
 def main():
-    thread1 = threading.Thread(target=listeners)
-    thread2 = threading.Thread(target=observers)
+    # thread1 = threading.Thread(target=listeners)
+    # thread2 = threading.Thread(target=observers)
 
-    thread1.start()
-    thread2.start()
+    # thread1.start()
+    # thread2.start()
 
-    thread1.join()
-    thread2.join()
+    # thread1.join()
+    # thread2.join()
+
+    listeners()
+
+def get_current_day():
+    year = datetime.now().year
+    month = datetime.now().month
+    day = datetime.now().day
+
+    return f"{year}-{month}-{day}"
+
+def analyze_keypress(key):
+    pass
 
 def test(window):
     for i in psutil.pids():
@@ -73,15 +160,28 @@ def test(window):
 
     
 if __name__ == "__main__":
-    # main()
+    # get_current_day()
+    main()
+    today_date = get_current_day()
 
-    active_window = gw.getActiveWindow()
-
-    if active_window:
-        window_type = test(active_window)
-        print("Active window type:", window_type)
+    with open('key_data.json', 'r') as js:
+        js_obj = json.load(js)
+    
+    if js_obj.get(today_date) != None:
+        print("exists")
     else:
-        print("No active window found.")
+        js.close()
+        new_json = {today_date: current_data}
+        with open('key_data.json', 'w') as js:
+            json.dump(new_json, js, indent=4)
+
+    # active_window = gw.getActiveWindow()
+
+    # if active_window:
+    #     window_type = test(active_window)
+    #     print("Active window type:", window_type)
+    # else:
+    #     print("No active window found.")
 
 
-    # print("tasks done")
+    print("tasks done")
